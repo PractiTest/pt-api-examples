1b7aba6a7e8f0026aac2c499b20947654127589e
Yaniv Iny <yaniv@practitest.com>
2017-03-09 10:02:12 +0200

    client with Response -> get, need to add authentication

---
+++ api.v2/java/src/main/java/com/practitest/examples/GetWithResponse.java
@@ -0,0 +1,54 @@
+package com.practitest.examples;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * This example demonstrates the use of the {@link ResponseHandler} to simplify
+ * the process of processing the HTTP response and releasing associated resources.
+ */
+public class GetWithResponse {
+  private static final String URI = "https://api.practitest.com/api/v2/projects.json";
+  private static final String DEVELOPER_EMAIL = "user@pt.com";
+  private static final String API_TOKEN = "XXX";
+
+    public final static void main(String[] args) throws Exception {
+
+        HttpClient httpclient = new DefaultHttpClient();
+
+        HttpGet request = new HttpGet(URI);
+
+        System.out.println("executing request " + request.getURI());
+
+        try {
+        // Create a response handler
+            HttpResponse response = httpclient.execute(request);
+            int statusCode = response.getStatusLine().getStatusCode();
+            HttpEntity entity = response.getEntity();
+            String responseBody = EntityUtils.toString(entity);
+            if (statusCode == 200) {
+                System.out.println("SUCCESS: " + responseBody);
+            } else {
+                System.out.println("ERROR: " + statusCode + ": " + responseBody);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            //  InputStream error = e.getErrorStream();
+            //  for (int i = 0; i < error.available(); i++) {
+            //    System.out.println("" + error.read());
+            //  }
+        }
+
+        System.out.println("----------------------------------------");
+
+        // When HttpClient instance is no longer needed,
+        // shut down the connection manager to ensure
+        // immediate deallocation of all system resources
+        httpclient.getConnectionManager().shutdown();
+    }
+
+}
